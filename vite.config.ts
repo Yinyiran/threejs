305ae326a7489777cb9482598dfd105c86724d08
yinyiran <Yinyiran@users.noreply.github.com>
2023-08-07 18:31:10 +0800

    chore: 增加自动引入等配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,39 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue', 'vue-router', 'pinia'],
+      eslintrc: {
+        enabled: true,
+        filepath: './plugins/.eslintrc-auto-import.json'
+      },
+      resolvers: [ElementPlusResolver()],
+      dts: './plugins/auto-imports.d.ts'
+    }),
+    Components({
+      dirs: ['src'],
+      resolvers: [ElementPlusResolver()],
+      dts: './plugins/components.d.ts'
+    })
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
-    port: 9090,
-    host: true,
-  },
+    port: 1000,
+    host: true
+  }
 })
