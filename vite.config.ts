3608c94a9329c7a8acf0ca9e4ae324329af40b74
yinyiran <Yinyiran@users.noreply.github.com>
2023-08-08 11:18:08 +0800

    chore: 依赖升级到最新；增加icon自动引入和自动安装

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
@@ -17,25 +22,26 @@ export default defineConfig({
       imports: ['vue', 'vue-router', 'pinia'],
       eslintrc: {
         enabled: true,
-        filepath: './plugins/.eslintrc-auto-import.json'
+        filepath: 'src/types/.eslintrc-auto-import.json',
       },
       resolvers: [ElementPlusResolver()],
-      dts: './plugins/auto-imports.d.ts'
+      dts: 'src/types/auto-imports.d.ts',
     }),
     Components({
       dirs: ['src'],
-      resolvers: [ElementPlusResolver()],
-      dts: './plugins/components.d.ts'
-    })
+      resolvers: [ElementPlusResolver(), IconsResolver()],
+      dts: 'src/types/components.d.ts',
+    }),
+    Icons({ autoInstall: true }),
   ],
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   server: {
     port: 1000,
-    host: true
-  }
+    host: true,
+  },
 })
